38df49face7d42a8b90e35d4ac035e9bed09ffb2
Thompson Plyler <nealok.g@gmail.com>
2025-01-06 15:57:47 -0500

    Attempts work with Netlify environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const handleSubmit = async (e) => {
-  e.preventDefault()
-  const response = await fetch('http://localhost:5000/test').then((res) => res.json())
-  console.log(response)
+const remote = process.env.VITE_REMOTE_HOST || import.meta.env.VITE_REMOTE_HOST
+const path = '/api/prompt_openai'
+console.log(remote)
 
-}
 
 function App() {
   const [count, setCount] = useState(0)
   const [body, setBody] = useState('')
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch(`${remote}${path}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: e.target.query.value
+      })
+    })
+      .then((res) => res.json())
+    console.log(response)
+    await setBody(response.result)
+  }
 
   return (
     <>
